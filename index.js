9b37fed45d589823bca6b1e4639cc363e1ecf88d
Lenny Wandeto <lenny.wandeto@student.moringaschool.com>
2024-04-01 10:44:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+ function findMatching(drivers, name){
+     const matched = drivers.filter((word) => word.toLowerCase()===name.toLowerCase())
+     return matched
+ }
+
+ function fuzzyMatch(drivers, name){
+     const matched = drivers.filter((word) => word.toLowerCase().startsWith(name.toLowerCase()))
+     return matched
+ }
+
+ function matchName(drivers, name){
+     const matched = drivers.filter((word) => word.name===name)
+     return matched
+ }
